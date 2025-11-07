29d5d45367dc5db5229dfe4b7af1b40078e1222a
sam <samsweeneyy@gmail.com>
2025-11-07 10:34:56 +1030

    final final

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,7 +30,7 @@
         </div>
 
         <div class="cta">
-          <a class="btn primary" href={`${base}/quiz/${q.slug}`} on:click|stopPropagation>
+          <a class="btn primary" href={`quizz/quiz/${q.slug}`} on:click|stopPropagation>
             Start
           </a>
         </div>
