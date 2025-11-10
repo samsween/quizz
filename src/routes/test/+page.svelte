a44631c27ee2249292f7cb24275b62708174ee68
sam <samsweeneyy@gmail.com>
2025-11-11 09:04:22 +1030

    Removed test page

--- src/routes/test/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import QuizRunner from "$lib/components/QuizRunner.svelte";
-    import { questions as q } from "$lib/quizzes/COMMUNICATE";
-</script>
-
-<QuizRunner questions={[q[1], q[14]]} page={1} slug={"eee"} />
