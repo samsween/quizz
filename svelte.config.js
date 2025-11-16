8d2fa0c33a8c2aa40b2ab9da2ac4421ee1a8633e
sam <samsweeneyy@gmail.com>
2025-11-16 21:54:01 +1030

    auto adap

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
 const dev = process.argv.includes('dev');
 
